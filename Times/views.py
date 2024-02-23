f190031aaf51847f5be7e58944a9072f33a496a9
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-02-23 19:53:56 -0300

    criacao template mostrar jogadores

--- Times/views.py
+++ Times/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Jogador
 
 # Create your views here.
 
 def exibirJogadores(request):
-    return HttpResponse('<h1>Jogadores</h1>')
+    jogadores = Jogador.objects.all()
+    context = {
+        'jogadores': jogadores
+    }
+    return render(request, 'Times/index.html', context)
