33062c5f2e5feea7df771d16b052c76298928115
Gisele <youremail@example.com>
2024-03-01 20:47:43 -0300

    Alterações para deploy 2

--- FutebolArte2/settings.py
+++ FutebolArte2/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,10 +90,7 @@ DATABASES = {
     }
 }
 
-
-
-
-
+# Postgresql remoto
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
